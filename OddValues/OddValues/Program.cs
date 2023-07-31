e81fad983ba5f545ef1aa41906be674623c6e67e
Abdur Rehman <abdurrehmanmughal1@gmail.com>
2023-07-31 15:37:15 +0500

    feat: display OddValues only

---
+++ OddValues/OddValues/Program.cs
@@ -0,0 +1,28 @@
+﻿public class program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = new int[5];
+        int i;
+
+        Console.WriteLine("enter the numbers");
+        for (i = 0; i < arr.Length; i++)
+        {
+            Console.Write("Element[" + (i + 1) + "]:");
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("List of odd  numbers : ");
+        for (i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] % 2 != 0)
+                Console.Write(arr[i] + " ");
+
+            //else
+            //{
+            //    Console.WriteLine("there is no odd no");
+            //}
+        }
+        Console.WriteLine(" ");
+    }
+}
